2f594efc541513a7fd84eeb05e14ba5cd47b0958
vuda <vuda@ghn.vn>
2019-10-25 23:01:05 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+const (
+	// TOKEN telegram
+	TOKEN = "904350232:AAHGK4iwOaKlr1ujT7FDdKeHLzYIwEQASVs"
+	// URL telegram
+	URL = "https://api.telegram.org/bot"
+	// PORT local
+	// PORT = os.Getenv("PORT")
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	// router := gin.New()
+	// router.Use(gin.Logger())
+	// router.LoadHTMLGlob("templates/*.tmpl.html")
+	// router.Static("/static", "static")
+
+	// router.GET("/", func(c *gin.Context) {
+	// 	c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	// })
+
+	// router.Run(":" + port)
+
+
+
+	http.HandleFunc("/api/v1/update", update)
+
+	fmt.Println("Listenning on port", port, ".")
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
