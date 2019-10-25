f55cb7a31a91d07f84ee2897f37930c1a9c159cb
vuda <vuda@ghn.vn>
2019-10-25 23:40:06 +0700

    update code

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-	"github.com/labstack/echo"
+	"encoding/json"
+	"fmt"
+	"github.com/DuongVu089x/golang-heroku/model"
 	"log"
 	"net/http"
 	"os"
@@ -11,46 +13,52 @@ import (
 
 const (
 	// TOKEN telegram
-	TOKEN = "904350232:AAHGK4iwOaKlr1ujT7FDdKeHLzYIwEQASVs"
+	TOKEN = "YOUR-TOKEN"
 	// URL telegram
 	URL = "https://api.telegram.org/bot"
 	// PORT local
-	// PORT = os.Getenv("PORT")
 )
-
-func main() {
-	port := os.Getenv("PORT")
-
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
-
-	// router := gin.New()
-	// router.Use(gin.Logger())
-	// router.LoadHTMLGlob("templates/*.tmpl.html")
-	// router.Static("/static", "static")
-
-	// router.GET("/", func(c *gin.Context) {
-	// 	c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	// })
-
-	// router.Run(":" + port)
-
-	//
-	//
-	//http.HandleFunc("/api/v1/update", update)
-	//
-	//fmt.Println("Listenning on port", port, ".")
-	//if err := http.ListenAndServe(":"+port, nil); err != nil {
-	//	log.Fatal(err)
-	//}
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Logger.Fatal(e.Start(":"+port))
-}
 
 func update(w http.ResponseWriter, r *http.Request) {
 
+	message := &model.ReceiveMessage{}
+
+	chatID := 0
+	msgText := ""
+
+	err := json.NewDecoder(r.Body).Decode(&message)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// if private or group
+	if message.Message.Chat.ID != 0 {
+		fmt.Println(message.Message.Chat.ID, message.Message.Text)
+		chatID = message.Message.Chat.ID
+		msgText = message.Message.Text
+	} else {
+		// if channel
+		fmt.Println(message.ChannelPost.Chat.ID, message.ChannelPost.Text)
+		chatID = message.ChannelPost.Chat.ID
+		msgText = message.ChannelPost.Text
+	}
+
+	respMsg := fmt.Sprintf("%s%s/sendMessage?chat_id=%d&text=Received: %s", URL, TOKEN, chatID, msgText)
+
+	// send echo resp
+	_, err = http.Get(respMsg)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func main() {
+	port :=  os.Getenv("PORT")
+
+	http.HandleFunc("/api/v1/update", update)
+
+	fmt.Println("Listenning on port", port, ".")
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
